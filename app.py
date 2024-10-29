fbbe571ac9e95ca66629e9ac3a06d85dfca016e8
hassan90122 <hassan90122@gmail.com>
2024-10-29 10:58:36 +0000

    Add Prometheus monitoring to Flask app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)  # Automatically adds a /metrics endpoint
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return "Hello, Giza Systems!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
